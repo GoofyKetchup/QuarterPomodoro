3b8e233ea7894f63d6eb910cf568b850623ba5a7
levraimerwane@gmail.com <levraimerwane@gmail.com>
2026-08-22 16:55:37 +0200

    Nope. Auto cycle won't release in this v1.1.

--- ui.py
+++ ui.py
@@ -157,7 +157,7 @@ def AddAutoCycleButtonToTheWindow(fg_color, text_color, font, button_command):
     global AutoCycleButton
     AutoCycleButton = CTkButton(
         ButtonsContainer3,
-        text="Auto Cycle",
+        text="Auto Cycle(not available yet)",
         fg_color=fg_color,
         text_color=text_color,
         font=(font, 20),
