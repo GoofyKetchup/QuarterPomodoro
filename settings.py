b06286c5d3b5f390d3545bafca58b0be80923560
levraimerwane@gmail.com <levraimerwane@gmail.com>
2026-08-26 18:46:15 +0200

    Added button to change ringtone(not working yet.)

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 from customtkinter import *
+from pathlib import Path
 
 def OpenSettings():
     swindow = CTkToplevel()
@@ -11,13 +12,13 @@ def OpenSettings():
         swindow,
         fg_color="#000000",
         text="  Configure settings here!  ",
-        font=("Arial", 15),
-        width=75,
-        height=25,
+        font=("Arial", 25),
+        width=100,
+        height=30,
         corner_radius=10,
         border_width = 1.5,
         border_color="red",
-    ).pack(side="top")
+    ).pack(side="top", pady=15)
 
     ParametersContainer1 = CTkFrame(swindow, fg_color="#000000")
     ParametersContainer1.pack(pady=5)
@@ -37,4 +38,22 @@ def OpenSettings():
     ParametersContainer6 = CTkFrame(swindow, fg_color="#000000")
     ParametersContainer6.pack(pady=5)
 
+    Change_Ringtone_Button = CTkButton(
+        ParametersContainer1,
+        text="Change ringtone",
+        fg_color="#000000",
+        text_color="#FFFFFF",
+        font=("Arial", 15),
+        corner_radius=10,
+        border_width=1.5,
+        border_color="red",
+        command=lambda: Change_Ringtone(),
+    )
+    Change_Ringtone_Button.pack(side="left", padx=5)
+
     swindow.mainloop()
+
+def Change_Ringtone():
+    # with open(f"{Path.cwd()}\\timer_finished.mp3", "w") as og:
+       # with open(filedialog.askopenfilename(initialdir="/", filetypes=("all files","*.*"), title="Choose your ringtone file."), "r") as new:
+        pass
